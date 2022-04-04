9a8cb0161b4ab0440ee835554185bd97006adf45
Kunsh Tyagi <k789.tyagi@gmail.com>
2022-04-04 20:39:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://tsprog.techsyndicate.us/logo.png"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>TECH SYNDICATE</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
